bd7812ca0443013d96e62425a7e5623d9941ca9b
lawanu <lawanu1@gmail.com>
2023-11-14 19:34:21 +0530

    mba all backend files added

---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+const userController = require('../controllers/user.controller');
+const { verifyUserReqBody, authJwt } = require("../middlewares");
+
+
+module.exports = function (app) {
+    app.get("/mba/api/v1/users/", [authJwt.verifyToken], userController.getAllUsers);
+    app.put("/mba/api/v1/users/", [authJwt.verifyToken], userController.update);
+    app.put("/mba/api/v1/users/:userId", [authJwt.verifyToken, authJwt.isAdmin, verifyUserReqBody.validateUserStatusAndUserType], userController.updateUser);
+
+
+}
